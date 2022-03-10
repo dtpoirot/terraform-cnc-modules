5c4bcb512439baeaa0baca22c8b488c672b716a1
Matthew Fenwick <mfenwick100@gmail.com>
2022-03-10 10:30:16 -0500

    streamline gcp deploy script

--- 2022.3/gcp/helm-chart/deploy.sh
+++ 2022.3/gcp/helm-chart/deploy.sh
@@ -13,24 +13,25 @@ Any modifications to this script will also not be supported.
 EOF
 
 
-GCP_PROJECT_ID=${GCP_PROJECT_ID:-}
-COVERITY_CLUSTER_NAME=${COVERITY_CLUSTER_NAME:-}
-COVERITY_CLUSTER_REGION=${COVERITY_CLUSTER_REGION:-}
-COVERITY_NS=${COVERITY_NS:-}
-COVERITY_PGHOST=${COVERITY_PGHOST:-}
-COVERITY_PGPASSWORD=${COVERITY_PGPASSWORD:-}
+# Required variables:
+# GCP_PROJECT_ID
+# COVERITY_CLUSTER_NAME
+# COVERITY_CLUSTER_REGION
+# COVERITY_NS
+# COVERITY_PGHOST
+# COVERITY_PGPASSWORD
+# COVERITY_GCS_SERVICE_ACCOUNT_FILE
+# COVERITY_HOST
+
 COVERITY_GCS_BUCKET_NAME=${COVERITY_GCS_BUCKET_NAME:-"${COVERITY_NS}-uploads-bucket"}
-COVERITY_GCS_SERVICE_ACCOUNT_FILE=${COVERITY_GCS_SERVICE_ACCOUNT_FILE:-}
-
-
-echo -e "\n===> Deploying prerequisites for COVERITY-UMBRELLA Helm Chart...\n"
-COVERITY_IMAGE_PULL_SECRET=${COVERITY_IMAGE_PULL_SECRET:-""}
-COVERITY_LICENSE_SECRET_NAME=${COVERITY_LICENSE_SECRET_NAME:-"coverity-license"}
-COVERITY_INITIALIZE_DB=${COVERITY_INITIALIZE_DB:-"true"}
-COVERITY_PGPORT=${COVERITY_PGPORT:-"5432"}
 COVERITY_PGUSER=${COVERITY_PGUSER:-"postgres"}
 COVERITY_GCS_SA_SECRET_NAME="cnc-gcs-credentials"
 COVERITY_GCS_SA_SECRET_KEY="key.json"
+COVERITY_INGRESS_SECRET_NAME="coverity-ingress"
+
+COVERITY_CIM_PGPASSWORD=$COVERITY_PGPASSWORD
+COVERITY_CIM_PGUSER=$COVERITY_PGUSER
+COVERITY_LICENSE_SECRET_NAME="coverity-license"
 
 
 ## Make sure your kubectl is pointing at the gcp cluster
@@ -54,43 +55,30 @@ kubectl create secret generic "${COVERITY_GCS_SA_SECRET_NAME}" \
   --dry-run -o yaml | kubectl apply -f -
 
 
-echo -e "\n===> Successfully installed prerequisites for COVERITY-UMBRELLA Helm Chart.\n"
+echo -e "\n===> Successfully created prerequisites for Coverity Helm Chart.\n"
 
 
-echo -e "\n===> Deploying COVERITY-UMBRELLA Helm Chart...\n"
-COVERITY_CIM_DATABASE="cim"
-COVERITY_CIM_PGPASSWORD=$COVERITY_PGPASSWORD
-COVERITY_CIM_PGUSER=$COVERITY_PGUSER
-COVERITY_SCAN_DATABASE="scan-jobs-service"
-COVERITY_STORAGE_DATABASE="storage-service"
+echo -e "\n===> Deploying Coverity Helm Chart...\n"
 
 
 helm upgrade "${COVERITY_NS}" --install \
-   "${COVERITY_CHART}" \
-   --debug \
-   --wait \
-   --timeout 15m0s \
-   --namespace "${COVERITY_NS}" \
-  --set imagePullSecret="${COVERITY_IMAGE_PULL_SECRET}" \
+  "${COVERITY_CHART}" \
+  --debug \
+  --wait \
+  --timeout 15m0s \
+  --namespace "${COVERITY_NS}" \
   --set licenseSecretName="${COVERITY_LICENSE_SECRET_NAME}" \
   --set postgres.user="${COVERITY_PGUSER}" \
   --set postgres.password="${COVERITY_PGPASSWORD}" \
   --set postgres.host="${COVERITY_PGHOST}" \
-  --set postgres.port="${COVERITY_PGPORT}" \
   --set cim.postgres.password="${COVERITY_CIM_PGPASSWORD}" \
   --set cim.postgres.user="${COVERITY_CIM_PGUSER}" \
-  --set cim.postgres.database=$COVERITY_CIM_DATABASE \
-  --set "cim.initializeJob.enabled=$COVERITY_INITIALIZE_DB" \
-  --set "cnc-storage-service.initializeJob.enabled=$COVERITY_INITIALIZE_DB" \
-  --set "cnc-scan-service.initializeJob.enabled=$COVERITY_INITIALIZE_DB" \
-  --set cnc-scan-service.postgres.database=$COVERITY_SCAN_DATABASE \
-  --set cnc-storage-service.postgres.database=$COVERITY_STORAGE_DATABASE \
   --set cnc-storage-service.gcs.bucket="${COVERITY_GCS_BUCKET_NAME}" \
   --set cnc-storage-service.gcs.secret.name="${COVERITY_GCS_SA_SECRET_NAME}" \
   --set cnc-storage-service.gcs.secret.key="${COVERITY_GCS_SA_SECRET_KEY}" \
   --set cim.ingress.hosts={"${COVERITY_HOST}"} \
-  --set cim.ingress.tls[0].secretName="cnc-ingress-tls" \
+  --set cim.ingress.tls[0].secretName="${COVERITY_INGRESS_SECRET_NAME}" \
   --set cim.ingress.tls[0].hosts={"${COVERITY_HOST}"} \
   "$@"
 
-echo -e "\n===> Successfully deployed COVERITY-UMBRELLA Helm Chart.\n"
+echo -e "\n===> Successfully deployed Coverity Helm Chart.\n"
